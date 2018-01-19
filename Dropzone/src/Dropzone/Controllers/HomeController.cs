04a3c03211233df89074f2e148e013dbd8d53607
HaoLuLu <1551540656@qq.com>
2018-01-19 11:13:14 +0800

    提交源代码

---
+++ Dropzone/src/Dropzone/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Dropzone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("/Views/Home/About.cshtml");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BatchUpload()
+        {
+            var files = Request.Form.Files;
+            bool isSavedSuccessfully = true;
+            int count = 0;
+            string msg = "";
+
+            string fileName = "";
+            try
+            {
+                string directoryPath = "Flie";//Path.GetDirectoryName("/Flie"); 
+                if (!Directory.Exists(directoryPath))
+                    Directory.CreateDirectory(directoryPath);
+
+                foreach (var file in files)
+                {
+                    var filename = ContentDispositionHeaderValue
+                                    .Parse(file.ContentDisposition)
+                                    .FileName
+                                    .Trim('"');
+                    filename = directoryPath + $@"\{filename}";
+                    using (FileStream fs = System.IO.File.Create(filename))
+                    {
+                        file.CopyTo(fs);
+                        fs.Flush();
+                    }
+                    count++;
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+                isSavedSuccessfully = false;
+            }
+
+            return Json(new
+            {
+                Result = isSavedSuccessfully,
+                Count = count,
+                Message = msg
+            });
+        }
+    }
+}
